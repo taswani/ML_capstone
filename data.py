a904e5aab674684df8776cf62a570f3929297714
Tanuj Aswani <tanuj.aswani@gmail.com>
2019-08-24 16:10:18 -0700

    cleaning and wrangling

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 amazon_df = pd.read_csv("combined_amazon_date_data.csv", index_col=False)
 
@@ -13,7 +14,12 @@ amzn_df['Date'] = pd.to_datetime(amzn_df['Date'], errors='coerce')
 amzn_df = amzn_df.dropna(subset=['Date'])
 
 # merge df on date
-result = pd.merge(amazon_df, amzn_df, how="left",  on='Date')
+result_df = pd.merge(amazon_df, amzn_df, how="left",  on='Date')
+
+#TODO Change timestamps to dates for sorting
+
+# cover null values in data
+result_df = result_df.fillna(method='ffill')
 
 # pushing df to a csv
-result.to_csv("final.csv")
+result_df.to_csv("final.csv")
