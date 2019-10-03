95dd943d86926605927f013ce87492252b49523f
Tanuj Aswani <tanuj.aswani@gmail.com>
2019-10-02 17:45:09 -0700

    updated with TODOS

--- linear_regression.py
+++ linear_regression.py
@@ -4,10 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#represent date in another way, dates are categorical (sklearn one-hot-encoding)
 X = result_df[['Date']].astype('datetime64').astype(int).astype(float)
 y = result_df[['Close']]
 lm = LinearRegression()
+#Train-test-split and crossvalidation - crossvalidate convenience function
 lm.fit(X, y)
-X_predict = lm.predict(X)
 
 plt.scatter(X, y)
+plt.show()
