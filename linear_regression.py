a8d6c8c0275f5be7887819607ee0e4fbbb078c41
Tanuj Aswani <tanuj.aswani@gmail.com>
2019-10-03 09:53:07 -0700

    added train_test_split

--- linear_regression.py
+++ linear_regression.py
@@ -1,5 +1,6 @@
 from data import result_df
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -9,7 +10,10 @@ X = result_df[['Date']].astype('datetime64').astype(int).astype(float)
 y = result_df[['Close']]
 lm = LinearRegression()
 #Train-test-split and crossvalidation - crossvalidate convenience function
-lm.fit(X, y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+lm.fit(X_train, y_train)
 
-plt.scatter(X, y)
+y_predict = lm.predict(X_test)
+plt.scatter(X_train, y_train)
+plt.plot(X_test, y_predict, color='red')
 plt.show()
