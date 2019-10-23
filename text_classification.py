cc64d3a3789d78ee7136849377a9acfcce4237b0
Tanuj Aswani <tanuj.aswani@gmail.com>
2019-10-22 21:49:40 -0700

    Updated TODO for sentiment analysis

--- text_classification.py
+++ text_classification.py
@@ -2,3 +2,5 @@ import data_pipeline as dp
 
 corpus = dp.vectorization(dp.prepare_data(dp.price_csv, dp.headline_csv))
 print(corpus)
+
+# TODO: Sentiment analysis via transfer learning since I have unlabled data at the moment.
