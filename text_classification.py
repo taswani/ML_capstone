af1263b6e04c8175f8e49414ada1e8a007aaf371
Tanuj Aswani <tanuj.aswani@gmail.com>
2019-11-01 13:15:01 -0500

    added jupyter notebook with text_classification

--- text_classification.py
+++ text_classification.py
@@ -4,7 +4,6 @@ from textblob import TextBlob
 result_df = dp.prepare_data(dp.price_csv, dp.headline_csv)
 processed_features = dp.vectorization(result_df)
 
-# TODO: Sentiment analysis via transfer learning since I have unlabeled data at the moment.
 # Look to use pre-trained model, aggregrate the predictions and append to result_df
 
 def sentiment_analysis(result_df, processed_features):
