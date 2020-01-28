64ef0faebb5dac97fc2dae404f774385cf8a9e5f
Tanuj Aswani <tanuj.aswani@gmail.com>
2020-01-27 17:15:22 -0800

    Updated TODOs

--- models/tests.py
+++ models/tests.py
@@ -9,5 +9,9 @@ class TestData(unittest.TestCase):
     def test_null_values(self):
         self.assertFalse(result_df.isnull().sum().sum())
 
+    # TODO: Testing the output given the right input
+    # Testing feature engineering
+    # Martin Fowler's testing pyramid is a good place to look at testing
+
 if __name__ == '__main__':
     unittest.main()
